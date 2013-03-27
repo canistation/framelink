888408fd41245e923dab956e04b172e8badacf5b
WolfLee <liyuan0228@gmail.com>
2013-03-27 21:46:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FrameLink (in theory) web app
+
+This app is aiming trendy people who want to use an different and easy
+way to show their own wedding albums on their own wedding and daily
+basis as target people.
+
+Some features that I want to implement here:
+
+- light & beautiful UI
+- sina weibo integration (using it as the photo storage actually)
+- easy & friendly interactivity
+- QR code support
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Framelink::Application.config.secret_token = '2a5aefaf65e0adffeafe0dbda4b9cdaf6673b7dd25babb810321e78d6e16cfe9930f45a252461eb7eabd0dc87e3a2f098258b06d9645af77ca805d81f2b33452'
